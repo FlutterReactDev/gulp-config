be6575fca4e78d88a9abc21ce3aba02f91bb3861
Den-codern <78246895+Den-codern@users.noreply.github.com>
2022-11-17 04:56:13 +0600

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,229 @@
+import gulp from "gulp";
+const { src, dest, watch, parallel, series } = gulp;
+import gulpSass from "gulp-sass";
+import nodeSass from "node-sass";
+    
+const sass = gulpSass(nodeSass);
+import prefixer from "gulp-autoprefixer";
+import clean from "gulp-clean-css";
+import concat from "gulp-concat";
+import map from "gulp-sourcemaps";
+import bs from "browser-sync";
+import chalk from "chalk";
+import uglify from "gulp-uglify-es";
+import babel from 'gulp-babel'
+import changed from "gulp-changed";
+import imagemin from "gulp-imagemin";
+import recompress from "imagemin-jpeg-recompress";
+import pngquant from "imagemin-pngquant";
+
+import webpConv from "gulp-webp";
+import multiDest from "gulp-multi-dest";
+import plumber from "gulp-plumber";
+import ttf2woff2 from "gulp-ttftowoff2";
+import ttf2woff from "gulp-ttf2woff";
+function browsersync() {
+	bs.init({ 
+		server: {
+			baseDir: 'build/',
+			host: '192.168.0.104',
+		},
+		
+		browser: 'default',
+		
+		logLevel: 'info',
+		logConnections: true,
+		logFileChanges: true,
+		open: true
+	})
+}
+function style() {
+  return src("src/scss/**/*.scss")
+    .pipe(map.init())
+    .pipe(
+        sass({
+        outputStyle: "compressed",
+      }).on("error", sass.logError)
+    )
+    .pipe(
+      prefixer({
+        overrideBrowserslist: ["last 8 versions"],
+        browsers: [
+          "Android >= 4",
+          "Chrome >= 20",
+          "Firefox >= 24",
+          "Explorer >= 11",
+          "iOS >= 6",
+          "Opera >= 12",
+          "Safari >= 6",
+        ],
+      })
+    )
+    .pipe(
+      clean({
+        level: 2,
+      })
+    )
+    .pipe(concat("style.min.css"))
+    .pipe(map.write("../sourcemaps/"))
+    .pipe(dest("build/css/"))
+    .pipe(bs.stream());
+}
+
+function libStyles(done) {
+  const plugins = ['src/lib/slick/slick.css','src/lib/slick/slick-theme.css'];
+  if (plugins.length > 0) {
+    return src(plugins)
+      .pipe(map.init())
+      .pipe(
+        sass({
+          outputStyle: "compressed",
+        }).on("error", sass.logError)
+      )
+      .pipe(concat("libs.min.css"))
+      .pipe(map.write("../sourcemaps/"))
+      .pipe(dest("build/css/"));
+  } else {
+    return done(
+      console.log(
+        chalk.bgYellow(
+          `${chalk.bold("WARNING!")} You did not add any CSS/SCSS plugins.`
+        )
+      )
+    );
+  }
+}
+
+function devJs() {
+  return src(["src/js/*.js"])
+    .pipe(map.init())
+    .pipe(uglify.default())
+    .pipe(concat("main.min.js"))
+    .pipe(map.write("../sourcemaps"))
+    .pipe(dest("build/js/"))
+    .pipe(bs.stream());
+}
+
+function libJs(done) {
+  const plugins = ['src/lib/slick/slick.min.js'];
+  if (plugins.length > 0)
+    return src(plugins)
+      .pipe(map.init())
+      .pipe(uglify.default())
+      .pipe(concat("libs.min.js"))
+      .pipe(map.write("../sourcemaps"))
+      .pipe(dest("build/js/"));
+  else {
+    return done(console.log(chalk.redBright("No added JS plugins")));
+  }
+}
+
+function buildJs() {
+  return src(["src/js/*.js"])
+    .pipe(uglify.default())
+    .pipe(
+      babel({
+        presets: ["@babel/env"],
+      })
+    )
+    .pipe(concat("main.min.js"))
+    .pipe(dest("build/js/"));
+}
+
+function rastr() {
+  return src("src/img/**/*.+(png|jpg|jpeg|gif|svg|ico)")
+    .pipe(changed("build/img"))
+    .pipe(
+      imagemin(
+        {
+          interlaced: true,
+          progressive: true,
+          optimizationLevel: 5,
+        },
+        [
+          recompress({
+            loops: 6,
+            min: 50,
+            max: 90,
+            quality: "high",
+            use: [
+              pngquant({
+                quality: [0.8, 1],
+                strip: true,
+                speed: 1,
+              }),
+            ],
+          }),
+          imagemin.gifsicle(),
+          imagemin.optipng(),
+          imagemin.svgo(),
+        ]
+      )
+    )
+    .pipe(dest("build/img"))
+    .pipe(bs.stream());
+}
+
+function webp() {
+  return src("build/img/**/*.+(png|jpg|jpeg)")
+    .pipe(plumber())
+    .pipe(
+      changed("build/img", {
+        extension: ".webp",
+      })
+    )
+    .pipe(webpConv())
+    .pipe(multiDest(["src/img", "build/img"]));
+}
+function fonts(done) {
+  src("src/fonts/**/*.ttf")
+    .pipe(
+      changed("build/fonts", {
+        extension: ".woff2",
+        hasChanged: changed.compareLastModifiedTime,
+      })
+    )
+    .pipe(ttf2woff2())
+    .pipe(dest("build/fonts"));
+
+  src("src/fonts/**/*.ttf")
+    .pipe(
+      changed("build/fonts", {
+        extension: "woff",
+        hasChanged: changed.compareLastModifiedTime,
+      })
+    )
+    .pipe(ttf2woff())
+    .pipe(dest("build/fonts"));
+  done();
+}
+function html() {
+  return src(["src/**/*.html"]).pipe(dest("build")).pipe(bs.stream());
+}
+function watching() {
+  watch("src/**/*.html", parallel(html));
+  watch("src/**/*.scss", parallel(style));
+  watch("src/**/*.js", parallel(devJs));
+  watch("src/img/**/*.+(png|jpg|jpeg|gif|svg|ico)", parallel(rastr));
+  watch("build/img/**/*.+(png|jpg|jpeg)", parallel(webp));
+  watch("src/fonts/**/*.ttf", parallel(fonts));
+}
+
+function build() {
+   return series(libStyles,fonts,style,libJs,buildJs,rastr,webp,html)
+}
+
+export { style, libStyles, devJs, libJs, buildJs, rastr, webp, fonts, html,watching,build };
+
+export default parallel(
+  libStyles,
+  style,
+  fonts,
+  devJs,
+  libJs,
+  rastr,
+  webp,
+  html,
+  browsersync,
+  watching
+);
---
+++ src/js/main.js
@@ -0,0 +1,445 @@
+$(document).ready(function () {
+  $("#menu").on("click", "a", function (event) {
+    //отменяем стандартную обработку нажатия по ссылке
+    event.preventDefault();
+
+    //забираем идентификатор бока с атрибута href
+    var id = $(this).attr("href"),
+      //узнаем высоту от начала страницы до блока на который ссылается якорь
+      top = $(id).offset().top;
+
+    //анимируем переход на расстояние - top за 1500 мс
+    $("body,html").animate({ scrollTop: top }, 1500);
+  });
+
+  $("#menu2").on("click", "a", function (event) {
+    //отменяем стандартную обработку нажатия по ссылке
+    event.preventDefault();
+
+    //забираем идентификатор бока с атрибута href
+    var id = $(this).attr("href"),
+      //узнаем высоту от начала страницы до блока на который ссылается якорь
+      top = $(id).offset().top;
+
+    //анимируем переход на расстояние - top за 1500 мс
+    $("body,html").animate({ scrollTop: top }, 1500);
+  });
+
+  const items = document.querySelectorAll(".item");
+
+  items.forEach((item) => {
+    item.addEventListener("click", () => {
+      item.classList.toggle("open");
+    });
+  });
+
+  $("ul.tabs li").click(function () {
+    var tab_id = $(this).attr("data-tab");
+
+    $("ul.tabs li").removeClass("current");
+    $(".tab-content").removeClass("current");
+
+    $(this).addClass("current");
+    $("#" + tab_id).addClass("current");
+  });
+
+  $(".slider_mini").slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+
+  $(".slider_products").slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: false,
+        },
+      },
+
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: false,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+
+  $(".header__slider").slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+
+  $(".navigation li").hover(function () {
+    var isHovered = $(this).is(":hover");
+    if (isHovered) {
+      $(this).children("ul").stop().slideDown(300);
+    } else {
+      $(this).children("ul").stop().slideUp(300);
+    }
+  });
+
+  const burger = document.querySelector(".burger");
+  const navbar = document.querySelector(".mt-mobile");
+  const body = document.querySelector("body");
+
+  burger.addEventListener("click", () => {
+    navbar.classList.toggle("nav-open");
+    body.classList.toggle("body-open");
+    burger.classList.toggle("burger-open");
+  });
+
+  $("div#tit1").click(function () {
+    $("div#more__bl1").css("display", "block");
+    $("div#tit1").css("display", "none");
+  });
+
+  $("div#tit2").click(function () {
+    $("div#more__bl2").css("display", "block");
+    $("div#tit2").css("display", "none");
+  });
+
+  $("div#tit3").click(function () {
+    $("div#more__bl3").css("display", "block");
+    $("div#tit3").css("display", "none");
+  });
+
+  $("div#tit4").click(function () {
+    $("div#more__bl4").css("display", "block");
+    $("div#tit4").css("display", "none");
+  });
+
+  $(".filtr__tit").on("click", function (e) {
+    $(".modul-1").css("display", "block");
+    $("body").css("overflow", "hidden");
+  });
+
+  $(".filtr__pop__tit").on("click", function (e) {
+    $(".modul-6").css("display", "block");
+    $("body").css("overflow", "hidden");
+  });
+
+  $("a.highslide").on("click", function (e) {
+    $(".modul-7").css("display", "flex");
+    $("body").css("overflow", "hidden");
+  });
+
+  $(".close__tits.close").on("click", function (e) {
+    $(".modul-1").css("display", "none");
+    $(".modul-6").css("display", "none");
+    $("body").css("overflow", "auto");
+  });
+
+  $(".closes").on("click", function (e) {
+    $(".modul-7").css("display", "none");
+    $("body").css("overflow", "auto");
+  });
+
+  $("a#is1").click(function () {
+    $("a#is1").css("display", "none");
+  });
+
+  $("a#is2").click(function () {
+    $("a#is2").css("display", "none");
+  });
+
+  $("a#is3").click(function () {
+    $("a#is3").css("display", "none");
+  });
+
+  $("a#is4").click(function () {
+    $("a#is4").css("display", "none");
+  });
+});
+
+$(".carts").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  fade: true,
+  autoplay: false,
+  arrows: false,
+  dots: false,
+  asNavFor: ".iss",
+});
+
+$(".iss").slick({
+  slidesToShow: 5,
+  vertical: true,
+  verticalSwiping: true,
+  slidesToScroll: 1,
+  asNavFor: ".carts",
+  active: true,
+  autoplay: false,
+  dots: false,
+  arrows: false,
+  centerPadding: "5px",
+  focusOnSelect: true,
+  responsive: [
+    {
+      breakpoint: 1260,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        dots: false,
+      },
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 5,
+        arrows: false,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 640,
+      settings: {
+        slidesToShow: 4,
+        dots: false,
+        arrows: false,
+        slidesToScroll: 1,
+      },
+    },
+
+    {
+      breakpoint: 565,
+      settings: {
+        slidesToShow: 6,
+        dots: false,
+        arrows: false,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
+
+$(".carts2").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  fade: true,
+  autoplay: false,
+  arrows: true,
+  dots: false,
+  asNavFor: ".iss2",
+});
+
+$(".iss2").slick({
+  slidesToShow: 6,
+  slidesToScroll: 1,
+  vertical: true,
+  verticalSwiping: true,
+  asNavFor: ".carts2",
+  active: true,
+  autoplay: false,
+  dots: false,
+  arrows: false,
+  centerPadding: "5px",
+  focusOnSelect: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        dots: false,
+      },
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 5,
+        arrows: false,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 640,
+      settings: {
+        slidesToShow: 4,
+        dots: false,
+        arrows: false,
+        slidesToScroll: 1,
+      },
+    },
+
+    {
+      breakpoint: 565,
+      settings: {
+        slidesToShow: 6,
+        dots: false,
+        arrows: false,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
+
+$(".carts3").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  fade: true,
+  autoplay: false,
+  arrows: false,
+  dots: false,
+  asNavFor: ".iss3",
+});
+
+$(".iss3").slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: ".carts3",
+  active: true,
+  autoplay: false,
+  dots: false,
+  arrows: true,
+  centerPadding: "5px",
+  focusOnSelect: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: false,
+        arrows: true,
+      },
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 3,
+        arrows: true,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 590,
+      settings: {
+        slidesToShow: 5,
+        dots: false,
+        arrows: true,
+        slidesToScroll: 1,
+      },
+    },
+
+    {
+      breakpoint: 420,
+      settings: {
+        slidesToShow: 5,
+        dots: false,
+        arrows: true,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
+
+var oldScrollY = 0;
+var div = document.getElementById("bottom__block");
+
+window.onscroll = function () {
+  var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+  var dY = scrolled - oldScrollY;
+
+  if (dY > 0) {
+    div.className = "bottom__block fixed-bottom";
+  } else {
+    div.className = "bottom__block fixed-top";
+  }
+
+  oldScrollY = scrolled;
+};
+
+$(".highslide").click(function () {
+  $(".slick.is.slick-slide.slick-active").click();
+});
+
+function hoverOnleft() {
+  document.getElementById("bg").classList.add("active");
+}
+
+function hoverOffleft() {
+  document.getElementById("bg").classList.remove("active");
+}
